68d88488d93ab5b7f8a5ea886f4a28fe5abe6daf
kenyofaria@gmail.com <kenyofaria@gmail.com>
2021-09-20 16:11:17 -0300

    sharing

---
+++ spring-crud/src/main/java/br/com/meli/controller/Professor.java
@@ -0,0 +1,29 @@
+package br.com.meli.controller;
+
+public class Professor {
+
+	private String primeiroNome;
+	private String ultimoNome;
+	
+	public Professor() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getPrimeiroNome() {
+		return primeiroNome;
+	}
+
+	public void setPrimeiroNome(String primeiroNome) {
+		this.primeiroNome = primeiroNome;
+	}
+
+	public String getUltimoNome() {
+		return ultimoNome;
+	}
+
+	public void setUltimoNome(String ultimoNome) {
+		this.ultimoNome = ultimoNome;
+	}
+	
+	
+}
